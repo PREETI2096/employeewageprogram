f9e96633061c49aedd79d8daa1e4b67c468b36ac
PREETI2096 <preetirg96@gmail.com>
2023-01-05 14:11:34 +0530

    [Preeti]: added UC2 calculating daily wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,20 +1,24 @@
 package com.bridgelabz.employeewage;
-
 import java.util.Random;
-
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-         System.out.println("Welcome to Employee wage computation program");
+System.out.println("Welcome to Employee wage computation program");
 		
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHour = 0;
+		int empWage = 0;
 		Random random = new Random();
 		int attendance = random.nextInt(2);
 		
 		if(attendance == IS_FULL_TIME)
-			System.out.println("Employee is present");
+			empHour = 8;
 		else 
-			System.out.println("Employee is absent");
+			empHour = 0;
+		empWage = empHour * EMP_RATE_PER_HOUR;
+		 
+		System.out.println("Daily Employee wage:" + empWage);
 
 	}
 }
