ade2a5e894785e9324c275baa6e417a31955ada6
PREETI2096 <108446384+PREETI2096@users.noreply.github.com>
2023-01-10 17:43:56 +0530

    Added : employee wage program

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -2,7 +2,6 @@ package com.bridgelabz.employeewage;
 import java.util.Random;
 public class EmployeeWage {
 
-	
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
