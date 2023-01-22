2180c5d3696d642f4544f789659b2ce9490e87e7
PREETI2096 <preetirg96@gmail.com>
2023-01-22 13:29:02 +0530

    Added : refactoring the employeewage program

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -2,16 +2,13 @@ package com.bridgelabz.employeewage;
 import java.util.Random;
 public class EmployeeWage {
 
-	
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HOURS_IN_MONTH = 100;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee wage computation program");
-		
+	public int wage() {
 		int empHour = 0;
 		int totalEmpHours = 0;
 		int totalWorkingDays = 0;
@@ -20,17 +17,17 @@ public class EmployeeWage {
 	    	totalWorkingDays++;
 	    	
 		Random random = new Random();
-		int attendance = random.nextInt(3);
+		int attendance = random.nextInt(3); //0,1,2
 		
 		switch(attendance) { 
-		case IS_PART_TIME  :
-		     	empHour = 4;
-		     	System.out.println("part time");
-			break;
-		
 		case IS_FULL_TIME :
 			empHour = 8;
 			System.out.println(" full time");
+			break;
+		
+		case IS_PART_TIME  :
+	     	empHour = 4;
+	     	System.out.println("part time");
 			break;
 			
 	    default: 
@@ -38,9 +35,16 @@ public class EmployeeWage {
 			System.out.println(" Absent");
 		}
 		totalEmpHours += empHour;
-		System.out.println("Day: "+totalWorkingDays+" =>" +" Emp Hours = "+ empHour);
+		System.out.println("Day: "+totalWorkingDays+"=>" +" Emp Hours= "+ empHour);
 		}
 	    int totalEmpWage = totalEmpHours *  EMP_RATE_PER_HOUR;
 		System.out.println("Total Employee wage:" + totalEmpWage);
+		return totalEmpWage;
+	}
+
+public static void main(String[] args) {
+	System.out.println("Welcome to Employee wage computation program");
+       EmployeeWage employeewage = new EmployeeWage();	
+       employeewage.wage();
 }
 }
